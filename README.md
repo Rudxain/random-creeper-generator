f29911f7cb8847956db9d2b251488c644d78b19e
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-08-20 18:06:47 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# RCG
+<img alt='Minimalist creeper face' src=res/icon.svg width=50% height=50%>
+
+# ▶️Demo
+## Desktop
+![](res/demo_horizontal.png)
+## Mobile
+![](res/demo_vertical.png)
+
+# ℹUsage
+You can [go to the website](https://Rudxain.github.io/random-creeper-generator) to see it on (almost) any device, or [⬇download](https://github.com/Rudxain/random-creeper-generator/archive/refs/heads/main.zip) this branch as a .zip file. If you downloaded the zip, then extract it. Open `src/index.html` in your 🌐browser of choice. Reload the page, maximize, un-maximize, rotate, resize, (etc.) and the background will change color. You can take screenshots in fullscreen to use them as wallpaper, or just "Save image as..." using your browser's context menu.
+
+# ⭐Credits
+1. Mojang and Microsoft. For developing such a wonderful game
+2. Markus Person "Notch". For accidentaly messing up a pig, lol
+
+The creeper is a creature/mob/entity in the game of Minecraft. It belongs to its corresponging owners and designers. I don't claim property of the creeper.
---
+++ src/main.js
@@ -0,0 +1,83 @@
+'use strict'
+const doc = document ,
+	canv = doc.getElementById('c') ,
+	ctx = canv.getContext( '2d', {alpha:false , desynchronized:true} ) ,
+	sign_abs = x => [Math.sign(x), Math.abs(x)] ,
+	//unary `+` is used to avoid accidental concat
+	randRange = (min=0 , max=1) => Math.random() * (max - min) + +min ,
+	clamp = (x, min, max) => x > max ? max : x < min ? min : x ,
+	//convert to u32 and return a B16 str whose max byte length is `B`
+	hexPad = (x=0, B=4) => (x >>> 0) .toString(0x10) .padStart(B << 1, '0')
+
+let w=0, h=0
+
+const settings = {
+	pixel_size : 0x40,
+	hue_variation : 1/16,
+	sat_variation : 1/8 * 100,
+	l_variation : 1/8 * 100,
+	resize_delay_ms : 1500
+}
+
+const resize = ()=>{
+	w = canv.width = doc.body.clientWidth
+	h = canv.height = doc.body.clientHeight
+
+	draw_bg()
+	draw_face()
+}
+
+const draw_bg = () => {
+	const {
+		pixel_size: size,
+		hue_variation: h_var,
+		sat_variation: s_var,
+		l_variation: l_var
+	} = settings
+
+	const hue_base = randRange(), sat_base = 100, l_base = 50
+
+	for (let y=0; y<h; y+=size)
+		for (let x=0; x<w; x+=size)
+		{
+			const hue_now = hue_base + randRange(-h_var, h_var),
+				sat_now = sat_base + randRange(-s_var, s_var),
+				l_now = l_base + randRange(-l_var, l_var)
+
+			ctx.fillStyle = `hsl(${hue_now}turn ${sat_now}% ${l_now}%)`
+			ctx.fillRect( x,y,size,size )
+		}
+}
+
+const draw_face = () => {
+	const size = settings.pixel_size,
+		size2 = size*2,
+		size3 = size*3,
+		center_x = w/2,
+		center_y = h/2
+
+	ctx.fillStyle = '#000'
+
+	//left eye
+	ctx.fillRect( center_x - size3, center_y - size2, size2, size2 )
+	//right eye
+	ctx.fillRect( center_x + size, center_y - size2, size2, size2 )
+	//mouth (center)
+	ctx.fillRect( center_x - size, center_y, size2, size3 )
+	//mouth (left)
+	ctx.fillRect( center_x - size2, center_y + size, size, size3 )
+	//mouth (right)
+	ctx.fillRect( center_x + size, center_y + size, size, size3 )
+}
+
+const main = ()=>{
+	let tm_ID
+	resize()
+
+	addEventListener('resize', ()=>{
+		clearTimeout( tm_ID )
+		tm_ID = setTimeout( resize, settings.resize_delay_ms )
+	})
+}
+if (typeof require == 'undefined' && typeof WorkerGlobalScope == 'undefined')
+	main()
